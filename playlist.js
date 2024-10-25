77768e3609d9a61d4095c5ab7e2ddfa9e1fd7051
tonis1000 <atonis@live.com>
2024-10-25 19:22:19 +0200

    Create playlist.js

---
+++ playlist.js
@@ -0,0 +1,6 @@
+const playlist = [
+    {
+        title: "Mega GR Stream",
+        url: "https://s2.cystream.net/live/megagr/chunks.m3u8?nimblesessionid=10004394&wmsAuthSign=c2VydmVyX3RpbWU9MTAvMjUvMjAyNCA0OjU0OjUwIFBNJmhhc2hfdmFsdWU9ZXRSOG8zT2FCU1NFbXF0QWRSR0pqUT09JnZhbGlkbWludXRlcz0yMA=="
+    }
+];
