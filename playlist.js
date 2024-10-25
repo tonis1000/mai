9338cfe193a42f164eb62fb19854732cd4157240
tonis1000 <atonis@live.com>
2024-10-25 19:26:51 +0200

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -2,5 +2,13 @@ const playlist = [
     {
         title: "Mega GR Stream",
         url: "https://s2.cystream.net/live/megagr/chunks.m3u8?nimblesessionid=10004394&wmsAuthSign=c2VydmVyX3RpbWU9MTAvMjUvMjAyNCA0OjU0OjUwIFBNJmhhc2hfdmFsdWU9ZXRSOG8zT2FCU1NFbXF0QWRSR0pqUT09JnZhbGlkbWludXRlcz0yMA=="
+    },
+        {
+        title: "Stream 2",
+        url: "https://example.com/path/to/anotherstream.m3u8"
+    },
+    {
+        title: "Stream 3",
+        url: "https://example.com/path/to/onemorestream.m3u8"
     }
 ];
