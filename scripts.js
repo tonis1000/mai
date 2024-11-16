bc1c03b1f44d0bfc19442e630acaa8491fa56ecf
tonis1000 <atonis@live.com>
2024-11-16 09:43:14 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ function loadMyPlaylist() {
 
 // Funktion zum Laden der externen Playlist und Aktualisieren der Sidebar
 function loadExternalPlaylist() {
-    fetch('https://raw.githubusercontent.com/jayp76/docs/refs/heads/main/misc/grlist.m3u')
+    fetch('https://raw.githubusercontent.com/gdiolitsis/greek-iptv/refs/heads/master/ForestRock_GR')
         .then(response => response.text())
         .then(data => updateSidebarFromM3U(data))
         .catch(error => console.error('Fehler beim Laden der externen Playlist:', error));
